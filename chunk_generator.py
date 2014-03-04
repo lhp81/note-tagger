f294a3ac07fe371b2833fd4de805e28fba45d35d
Luke Petschauer <lpetschauer@gmail.com>
2014-03-04 13:04:27 -0800

    update chunker and test

--- chunk_generator.py
+++ chunk_generator.py
@@ -4,13 +4,18 @@ import nltk, re, pprint
 """
 About:
 ===================
-ongoing work on the chunk generator. this is something that will go through a
+ongoing work on our chunk generator. this is something that will go through a
 text and find the most common noun phrases. the idea behind this is that noun
 phrases that occur with higher frequency will probably be important and reflect
 what the note is about.
 this is something to be extended; probably we would want to implement a related
 words search or something like that on the most common words, seeing if that
 will help to bring up more general keywords.
+
+The below was inspired by:
+Zhu, D. "Text Chunking using NLTK". Online at:
+    http://www.eecis.udel.edu/~trnka/CISC889-11S/lectures/dongqing-chunking.pdf
+The NLTK Book. Online at: http://nltk.org/book
 """
 
 
@@ -20,38 +25,25 @@ Plan of attack:
 First: Segment, tokenize, and tag, using the NLTK's resources.
 Second: Chunk using the predetermined patterns.
 Third: Return the most common chunks.
-Fourth: Profit(?)
+Fourth: Profit. Failing that, find a prophet to follow. First choice > second.
 """
 
 
 # to-dos:
-# 1. create a chunk parser by using predefined NP chunks
-# 2. parse every sentence
 # 3. store NP chunks
 # 4. extract keywords based upon frequency
 
 
 """First: Segment, tokenize, and tag, using the NLTK's resources."""
 
-# this is to prepare our text for chunk parsing
-
-
-def note_preprocess(input):
-    sentences = nltk.sent_tokenize(input)
-    sentences = [nltk.word_tokenize(sent) for sent in sentences]
-    sentences = [nltk.pos_tag(sent) for sent in sentences]
-# the above is based upon Zhu, D. Text Chunking using NLTK. Online at:
-# http://www.eecis.udel.edu/~trnka/CISC889-11S/lectures/dongqing-chunking.pdf
-
-
+# wrapped up in the below.
 
 """Second: Chunk using predetermined patterns"""
-
 
 # set the patterns we want to look for
 patterns = """
     NP: {<JJ>*<NN>+}
-    {<JJ>*<NNP>?<NN>+}
+    {<JJ>*<NN*>?<NN>+}
     {<NNP>+<NNPS>+}
     {<NN>+<NNS>+}
     {<NN>?<NNP>*<CD>}
@@ -62,25 +54,31 @@ patterns = """
     # {<NNP>+}
     # {<NN>+}
 
-#  the above needs to be extended after I can see real world results
+# the above needs to be extended after I can see real world results
+# may want to start using <NN*> as a choice.
 
-# create a chunk parser that uses the above patterns.
+# create a chunk parser that works using the above patterns.
 NPChunker = nltk.RegexpParser(patterns)
 
 
 # this will parse the sentences, starting our NP identification process.
-def sentence_parser(sentences):
+def sentence_parser(input):
     results = []
+    sentences = nltk.sent_tokenize(input)
+    sentences = [nltk.word_tokenize(sent) for sent in sentences]
+    sentences = [nltk.pos_tag(sent) for sent in sentences]
     for sent in sentences:
         result = NPChunker.parse(sent)
         results.append(result)
-    for result in results:
-        print result
 
+
+"""Third: Return the most common chunks."""
 
 # a tree traversal function for extracting NP chunks in the parsed tree
 # so this will go through the results from sentence_parser and just pick out
 # the NP.
+
+
 def traverse(t):
     cool_nps = []
     try:
@@ -94,10 +92,3 @@ def traverse(t):
             for child in t:
                 traverse(child)
     return cool_nps
-
-
-# three above inspired by Zhu, D. Text Chunking using NLTK. Online at:
-# http://www.eecis.udel.edu/~trnka/CISC889-11S/lectures/dongqing-chunking.pdf
-
-
-"""Third: Return the most common chunks."""
--- chunk_generator_test.py
+++ chunk_generator_test.py
@@ -1,4 +1,51 @@
-import nltk
+import nltk, re, unittest
 from chunk_generator import *
 
 
+class ChunkGeneratorConstituentsTest(unittest.TestCase):
+    """ testing the various parts of the chunker"""
+
+    def test_sentence_parser(self):
+        sentences = "Four score and seven years ago, our fathers brought \
+            upon this continent a new nation, conceived in liberty \
+            and dedicated to the proposition that all men are created \
+            equal. Now we are engaged in a great civil war, testing \
+            whether that nation, or any nation so conceived and so \
+            dedicated, can long endure."
+        sentences = nltk.sent_tokenize(sentences)
+        self.assertEqual(len(sentences), 2)
+
+    def test_sentence_tagger(self):
+        sentences = "Four score and seven years ago, our fathers brought \
+            upon this continent a new nation, conceived in liberty \
+            and dedicated to the proposition that all men are created \
+            equal. Now we are engaged in a great civil war, testing \
+            whether that nation, or any nation so conceived and so \
+            dedicated, can long endure."
+        sentences = nltk.sent_tokenize(sentences)
+        sentences = [nltk.word_tokenize(sent) for sent in sentences]
+        self.assertEqual(len(sentences[0]), 32)
+        # this is 32 because there are 32 distinct items in the first sentence,
+        # where items are words and pieces of punctuation.
+        # sent_tokenize creates two lists, word_tokenize breaks sentences up
+        # into words.
+
+    def test_word_tagger(self):
+        sentences = "Four score and seven years ago, our fathers brought \
+            upon this continent a new nation, conceived in liberty \
+            and dedicated to the proposition that all men are created \
+            equal. Now we are engaged in a great civil war, testing \
+            whether that nation, or any nation so conceived and so \
+            dedicated, can long endure."
+        sentences = nltk.sent_tokenize(sentences)
+        sentences = [nltk.word_tokenize(sent) for sent in sentences]
+        sentences = [nltk.pos_tag(sent) for sent in sentences]
+        self.assertEqual(sentences[0][1], ('score', 'NN'))
+        # check that the second word of the address, 'score' is correctly
+        # tagged as a noun. Different taggers were doing different things with
+        # the word 'four'...so I went with something safe.
+
+
+
+if __name__ == '__main__':
+    unittest.main()
