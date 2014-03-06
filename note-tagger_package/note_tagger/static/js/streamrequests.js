129ba3e44ed6d320c6bfaf56162c208a3abd14ad
Jack Markley <jack.markley@gmail.com>
2014-03-06 23:35:32 +0100

    animooted and formutted

--- note-tagger_package/note_tagger/static/js/streamrequests.js
+++ note-tagger_package/note_tagger/static/js/streamrequests.js
@@ -8,11 +8,14 @@ $(document).ready(function(){
             type: "GET",
             dataType: "json",
             success: function(data) {
-                $('#thestream').append("<br/>Tags:");
+                $('#thestream').append("<br/><br/>Tags:");
                 for(var i=1; i<data.length;i++){
                     $('#thestream').append(' '+data[i]);
                 }
                 $('#thestream').append(".<br/>"+data[0]);
+                $('#thestream').stop().animate({
+                    scrollTop: $('#thestream')[0].scrollHeight
+                }, 800);
             }
         });
     });
