26f7743c9c2dfe5ece691d2cbc17fd420a3422a3
Jack Markley <jack.markley@gmail.com>
2014-03-07 01:24:19 +0100

    prettier page

--- note-tagger_package/note_tagger/static/js/streamrequests.js
+++ note-tagger_package/note_tagger/static/js/streamrequests.js
@@ -16,7 +16,7 @@ $(document).ready(function(){
                 $('#thestream').stop().animate({
                     scrollTop: $('#thestream')[0].scrollHeight
                 }, 800);
-                $('#thestream').value("");
+                $('#noteentry').val("");
             }
         });
     });
